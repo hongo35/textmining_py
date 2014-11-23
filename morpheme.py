d740f3334c45839897103bdab7d4ba6d403c000b
Yutaka Hongo <hongo@userlocal.jp>
2014-11-23 19:08:18 +0900

    [add]mecab-pythonで形態素解析

--- morpheme.py
+++ morpheme.py
@@ -1,7 +1,34 @@
 # -*- coding: utf-8 -*-
+import sys
+sys.path.append('config')
+
 import MeCab
+import MySQLdb as mysql
+import config
 
-mecab = MeCab.Tagger()
+def main():
+	# db接続
+	con = mysql.connect(
+		host    = config.db['host'],
+		user    = config.db['user'],
+		passwd  = config.db['passwd'],
+		db      = config.db['db'],
+		charset = 'utf8'
+	)
+	cur = con.cursor()
 
-node = mecab.parseToNode("今日もしないとね")
-print node
+	mecab = MeCab.Tagger()
+
+	cur.execute("SELECT body FROM favorites LIMIT 10")
+	res = cur.fetchall()
+	for r in res:
+		node = mecab.parseToNode(r[0].encode("utf-8"))
+		while node:
+			print node.surface + '\t' + node.feature
+			node = node.next
+
+	#node = mecab.parseToNode("今日もしないとね")
+	#print node
+
+if __name__ == '__main__':
+	main()
