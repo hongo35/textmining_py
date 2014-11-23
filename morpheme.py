1c66a01cc59309fc264c160bbc7a6ff6ac1a02ba
hongo35 <s.style3.5@gmail.com>
2014-11-23 20:56:53 +0900

    [add]favツイートから特徴語を抽出

--- morpheme.py
+++ morpheme.py
@@ -5,6 +5,7 @@ sys.path.append('config')
 import MeCab
 import MySQLdb as mysql
 import config
+import collections
 
 def main():
 	# db接続
@@ -19,16 +20,25 @@ def main():
 
 	mecab = MeCab.Tagger()
 
-	cur.execute("SELECT body FROM favorites LIMIT 10")
+	cur.execute("SELECT body FROM favorites lIMIT 100")
 	res = cur.fetchall()
-	for r in res:
-		node = mecab.parseToNode(r[0].encode("utf-8"))
-		while node:
-			print node.surface + '\t' + node.feature
-			node = node.next
 
-	#node = mecab.parseToNode("今日もしないとね")
-	#print node
+	fav_nouns = collections.defaultdict(int)
+	for r in res:
+		text_s = r[0].split()
+		for ts in text_s:
+			if not (ts.startswith("http") or ts.startswith("RT") or ts.startswith("@")):
+				node = mecab.parseToNode(ts.encode("utf-8"))
+				while node:
+					category = node.feature.split(",")[0]
+					if category == "名詞" and len(unicode(node.surface, 'utf-8')) > 1:
+						if config.stop_noun.find(node.surface) == -1:
+							fav_nouns[node.surface] += 1
+
+					node = node.next
+
+	for k,v in sorted(fav_nouns.items(), key=lambda x: x[1]):
+		print "%s: %d", k,v
 
 if __name__ == '__main__':
 	main()
